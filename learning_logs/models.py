975b3dd86ac5156df7ad58486651ef3c98531b8e
mashen <mashen@dintech.com>
2018-12-28 19:22:58 +0800

    edit entry

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -13,7 +13,7 @@ class Topic(models.Model):
 
 
 class Entry(models.Model):
-    '''学到的有关某个主题的具体指示'''
+    '''学到的有关某个主题的具体条目'''
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
     text = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
